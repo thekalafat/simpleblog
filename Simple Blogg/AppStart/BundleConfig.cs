7633ac7d1784815930ed5399acf7606220e5cbac
berkalafat <berkalafat@outlook.com>
2019-04-15 13:32:43 +0300

    eyyo

---
+++ Simple Blogg/AppStart/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlogg.AppStart
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css")
+                );
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/style.css")
+                );
+        }
+    }
+}
--- Simple Blogg/Global.asax.cs
+++ Simple Blogg/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SimpleBlogg.AppStart;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Simple_Blogg
@@ -13,6 +15,7 @@ namespace Simple_Blogg
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
